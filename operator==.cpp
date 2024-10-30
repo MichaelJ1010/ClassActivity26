31d4eca66568c7261cc0f76c616086b4aaca64fa
Michael J <30031129@cc.peralta.edu>
2024-10-29 18:03:03 -0700

    overloaded == operator

---
+++ operator==.cpp
@@ -0,0 +1,5 @@
+#include "./operator==.hpp"
+
+bool operator==(coordinate::Point point1, coordinate::Point point2) {
+	return (point1.x == point2.x && point1.y == point2.y);
+}
--- operator==.hpp
+++ operator==.hpp
@@ -2,7 +2,7 @@
 #define OPERATOR==_H
 
 namespace equals{
-	bool operator==(coordinate::Point point);
+	bool operator==(coordinate::Point point1, coordinate::Point point2);
 }
 
 #endif
