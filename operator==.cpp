6f06be3d65dc3ada5efa9db2460229a7abf71c77
Michael J <30031129@cc.peralta.edu>
2024-10-29 18:04:01 -0700

    included correct hpp files to code

--- operator==.cpp
+++ operator==.cpp
@@ -1,4 +1,5 @@
 #include "./operator==.hpp"
+#include "./point.hpp"
 
 bool operator==(coordinate::Point point1, coordinate::Point point2) {
 	return (point1.x == point2.x && point1.y == point2.y);
--- operator==.hpp
+++ operator==.hpp
@@ -1,6 +1,8 @@
 #ifndef OPERATOR==_H
 #define OPERATOR==_H
 
+#include "./point.hpp"
+
 namespace equals{
 	bool operator==(coordinate::Point point1, coordinate::Point point2);
 }
