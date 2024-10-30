0f34f77121e70fa5b6a9899c0d56270c765800d4
Michael J <30031129@cc.peralta.edu>
2024-10-29 20:44:20 -0700

    added header file to check points

---
+++ checkPoints.hpp
@@ -0,0 +1,9 @@
+#ifndef CHECKPOINTS_H
+#define CHECKPOINTS_H
+
+#include "./point.hpp"
+
+namespace point {
+	void checkPoints(const coordinate::Point& point1, const coordinate::Point& point2);
+}
+#endif
